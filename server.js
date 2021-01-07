ae7e4aa7815761fd8b587b02f991b7c35a92a41d
dimitrycodes <strongsafetyking@yahoo.com>
2021-01-08 01:42:52 +0600

    feat: token validation

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
+
+const { API_TOKEN, PORT } = process.env;
+
+function validateBearerToken(req, res, next) {
+  const apiToken = API_TOKEN;
+  const authToken = req.get('Authorization');
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+    return res.status(401).json({ error: 'Unauthorized request' });
+  }
+  next();
+}
 
 const app = express();
 
 app.use(morgan('dev'));
 
+app.use(validateBearerToken);
+
 app.use((req, res) => {
   res.send('Hello, world!');
 });
 
-const PORT = 5000;
+const port = PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server listening at http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
